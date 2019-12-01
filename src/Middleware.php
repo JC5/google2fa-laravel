5018619ff40d804a898bafe72b05bd35f4a41d1f
James Cole <thegrumpydictator@gmail.com>
2019-12-01 13:00:06 +0100

    Middleware checks for cookie, and sets one if necessary.

--- src/Middleware.php
+++ src/Middleware.php
@@ -3,16 +3,37 @@
 namespace PragmaRX\Google2FALaravel;
 
 use Closure;
+use Illuminate\Http\Response;
 use PragmaRX\Google2FALaravel\Support\Authenticator;
+use PragmaRX\Google2FALaravel\Support\Constants;
 
 class Middleware
 {
     public function handle($request, Closure $next)
     {
+        /** @var Authenticator $authenticator */
         $authenticator = app(Authenticator::class)->boot($request);
+        $cookieResult  = $authenticator->hasValidCookieToken();
+        $authResult    = $authenticator->isAuthenticated();
+        /** @var Response $response */
+        $response = $next($request);
 
-        if ($authenticator->isAuthenticated()) {
-            return $next($request);
+        // auth but no cookie? place one:
+        if (false === $cookieResult && true === $authResult) {
+
+            // set cookie with token:
+            $cookieName = config('google2fa.cookie_name') ?? 'google2fa_token';
+            $lifetime   = (int)(config('google2fa.cookie_lifetime') ?? 8035200);
+            $lifetime   = $lifetime > 8035200 ? 8035200 : $lifetime;
+            $token      = $authenticator->sessionGet(Constants::SESSION_TOKEN);
+            $response->withCookie(cookie()->make($cookieName, $token, $lifetime / 60));
+        }
+        if (true === $cookieResult) {
+            return $response;
+        }
+
+        if (true === $authResult) {
+            return $response;
         }
 
         return $authenticator->makeRequestOneTimePasswordResponse();
